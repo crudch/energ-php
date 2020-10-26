62f8fc77672cfe1cecde9174df40bd666d7ebc4c
Energyuniverse <newenergy1983@gmail.com>
2020-10-26 23:58:08 +0200

    Task 2.1.1 (Заполнение лота с товаром)fix

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ $products = [
                         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $key["title"] ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
-                                <span class="lot__amount"><?= $key["price"] ?></span><!--ЧТО ТУТ ПИСАТЬ ТО!-->
+                                <span class="lot__amount">Стартовая цена</span><!--ЧТО ТУТ ПИСАТЬ ТО!-->
                                 <span class="lot__cost"><?= $key["price"] ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
